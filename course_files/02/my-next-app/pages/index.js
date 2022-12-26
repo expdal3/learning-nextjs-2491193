2e81304faf01c09378c6ea0af554303642d1a0e5
Andre Le <53202814+expdal3@users.noreply.github.com>
2022-12-26 19:38:12 +1100

    v.01.03.1537 use <Link> tag for nav

--- course_files/02/my-next-app/pages/index.js
+++ course_files/02/my-next-app/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'    //import link to enable navigation
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -20,13 +21,14 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+        
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+        <Link href="/learn/next">
+          <a className={styles.card}>
+            <h2>Learn NextJS &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
+          </Link>
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
