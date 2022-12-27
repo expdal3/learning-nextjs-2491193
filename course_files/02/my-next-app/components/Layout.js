e2fc2d5badf78f67eb7f4c7f05e245a61a40478a
Andre Le <53202814+expdal3@users.noreply.github.com>
2022-12-27 15:14:51 +1100

    getStaticProp .01

--- course_files/02/my-next-app/components/Layout.js
+++ course_files/02/my-next-app/components/Layout.js
@@ -1,21 +1,31 @@
 import Link from "next/link"
 import styles from '../styles/Home.module.css'
 
-/* Menu Component here */
-function Menu() {           //<-- header nav menu
+
+export default function Layout({ children }) {
     return(
-        <ul>
-            <Link href="/"><a>Home</a></Link>&nbsp;
-            <Link href="/contacts"><a>Contacts</a></Link>
-        </ul>
+        <>
+         <Link href="/"><a>Home</a></Link>
+          {children}
+        </>
     )
 }
 
-export default function Layout({children}) {
-    return(
-        <div className={styles.container}>
-            {/* insert Menu here */}
-            <Menu />
-            {children}
-        </div>)
-}
+/* Menu Component here */
+// function Menu() {           //<-- header nav menu
+//     return(
+//         <ul>
+//             <Link href="/"><a>Home</a></Link>&nbsp;
+//             <Link href="/contacts"><a>Contacts</a></Link>
+//         </ul>
+//     )
+// }
+
+// export default function Layout({children}) {
+//     return(
+//         <div className={styles.container}>
+//             {/* insert Menu here */}
+//             <Menu />
+//             {children}
+//         </div>)
+// }
