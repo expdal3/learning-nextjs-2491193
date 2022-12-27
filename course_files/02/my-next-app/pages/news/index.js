a4f9e3acdfcc993ad6905838d5f76f74df3ffce6
Andre Le <53202814+expdal3@users.noreply.github.com>
2022-12-28 10:52:26 +1100

    v.02.01.1052 complete getStaticProps

--- course_files/02/my-next-app/pages/news/index.js
+++ course_files/02/my-next-app/pages/news/index.js
@@ -1,16 +1,38 @@
-import Layout from "../../components/Layout"
-export default function News() {
-    return(
+import Layout from "../../components/Layout";
+
+export default function News( {results} ) {
+  return (
     <Layout>
       <h1>Top Stories</h1>
-    </Layout>)
+      <ul>
+        {results.map((result) => {
+          return (
+            <li key={result.uri}>
+              <a href={result.url} target="_blank" rel="noopener norefferer"> 
+                {" "}
+                {result.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
 }
 
-const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
-export async function getStaticProps() {
+const API_KEY = "GbH3qJlJ1MOLiVb2TqQQ2Fnm3AmfLxga";
+export async function getStaticProps() { 
+  // getStaticProps pre-rendering the data in the back for faster page load, this is run on the server-side only at build time
+  // fetch response from ny-api
+  const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data.results);
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
-    props: {}
-  }
+    props: {
+      results: data.results,
+    },
+  };
 }
